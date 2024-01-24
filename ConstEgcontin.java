28ddda67ef24e48d338b8568c6fa0ae3001506ec
PragnasriBendi <113855732+PragnasriBendi@users.noreply.github.com>
2024-01-24 20:58:24 +0530

    Add files via upload
    Basic constructor Example

---
+++ ConstEgcontin.java
@@ -0,0 +1,11 @@
+package demopack;
+
+public class ConstEgcontin {
+
+	public static void main(String[] args) {
+		ConstructorEg eg = new ConstructorEg(1111, "pinky", 'B');
+		eg.display();
+
+	}
+
+}
---
+++ ConstructorEg.java
@@ -0,0 +1,20 @@
+package demopack;
+
+public class ConstructorEg {
+	
+	int sid;
+	String sname;
+	char sgrade;
+	
+	public void display() {
+		System.out.println(sid+" "+sname+" "+sgrade);
+	}
+	
+	ConstructorEg(int id, String name, char g){
+		sid=id;
+		sname=name;
+		sgrade=g;
+		
+	}
+
+}
